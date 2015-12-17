1969b11feeafd4101d7fae0cda11581478520aac
alonso <hector.martinez.a@gmail.com>
2015-12-17 16:24:07 +0100

    x

--- src/generate_tokenized_file.py
+++ src/generate_tokenized_file.py
@@ -10,14 +10,14 @@ def main():
     nam=re.compile("name=([\w_]+)")
     acc=""
     for linepred, linefeats in zip(open(args.preds,encoding="utf-8").readlines(),open(args.feats,encoding="utf-8").readlines()):
-        print(linepred,linefeats)
         linepred = linepred.strip()
         if linepred:
+            linepred = linepred.split()
             if nam.search(linefeats).group(0):
                 ud_name = nam.search(linefeats).group(0).replace("name=","").replace("_"," ")
             pred = linepred[2][2:-1] #b'2' --> 2
             if pred == "1":
-                acc=" "
+                acc+=" "
             acc+=ud.lookup(ud_name)
         else:
             #"end of line"
