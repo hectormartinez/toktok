f0045a52df1e1810d53a631a5142cf4a91f547fc
alonso <hector.martinez.a@gmail.com>
2015-12-17 14:31:07 +0100

    x

--- src/train_with_all_and_tokenize.sh
+++ src/train_with_all_and_tokenize.sh
@@ -5,9 +5,9 @@ lang_short=( ar bg cs da de en es eu fa fi fr he hi hr id it nl no pl pt sl sv)
 for v in 0 #1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22
 do
     l_short=${lang_short[$v]}
-    python3.4 generate_features.py --train_file $treebankbasepath/"$l_short"-ud-train.conllu.lex > "$l_short".all.train.tokf
-    python3.4 generate_features.py  --train_file $treebankbasepath/"$l_short"-ud-all.conllu.lex --test_file $rawtextbasepath/bible/"$l_short" > "$l_short".bible.tokf
-    python3.4 generate_features.py  --train_file $treebankbasepath/"$l_short"-ud-all.conllu.lex --test_file $rawtextbasepath/watchtower/"$l_short" > "$l_short".watchtower.tokf
+    python2.7 generate_features.py --train_file $treebankbasepath/"$l_short"-ud-train.conllu.lex > "$l_short".all.train.tokf
+    python2.7 generate_features.py  --train_file $treebankbasepath/"$l_short"-ud-all.conllu.lex --test_file $rawtextbasepath/bible/"$l_short" > "$l_short".bible.tokf
+    python2.7 generate_features.py  --train_file $treebankbasepath/"$l_short"-ud-all.conllu.lex --test_file $rawtextbasepath/watchtower/"$l_short" > "$l_short".watchtower.tokf
     rungsted --train "$l_short".all.train.tokf -f $l_short.tok.mdl
     rungsted --test "$l_short".watchtower.tokf -i $l_short.tok.mdl
     rungsted --test "$l_short".bible.tokf -i $l_short.tok.mdl
