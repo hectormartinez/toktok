42ca38fdb5977161cfe144c69af8df358a422306
alonso <hector.martinez.a@gmail.com>
2015-12-17 17:37:35 +0100

    also cs and no debug

--- src/train_with_all_and_tokenize.sh
+++ src/train_with_all_and_tokenize.sh
@@ -1,13 +1,13 @@
 treebankbasepath=$1
 rawtextbasepath=$2
-lang_short=(  fa en es et eu fi fr ga ar bg cs da de el  got grc he hi hr hu id it la nl no pl pt ro sl sv ta )
+lang_short=( ar bg cs da de el en es et eu fa fi fr ga got grc he hi hr hu id it la nl no pl pt ro sl sv ta)
 #lang_short=( ar bg cs da de en es eu fa fi fr he hi hr id it nl no pl pt sl sv)
-for v in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22
+for v in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
 do
     l_short=${lang_short[$v]}
-    python generate_features.py --train_file $treebankbasepath/"$l_short"-ud-train.conllu.lex --debug > "$l_short".all.train.tokf
-    python generate_features.py  --train_file $treebankbasepath/"$l_short"-ud-all.conllu.lex --debug --to_tok $rawtextbasepath/bible/"$l_short" > "$l_short".bible.tokf
-    python generate_features.py  --train_file $treebankbasepath/"$l_short"-ud-all.conllu.lex --debug --to_tok $rawtextbasepath/watchtower/"$l_short" > "$l_short".watchtower.tokf
+    python generate_features.py --train_file $treebankbasepath/"$l_short"-ud-train.conllu.lex  > "$l_short".all.train.tokf
+    python generate_features.py  --train_file $treebankbasepath/"$l_short"-ud-all.conllu.lex --to_tok $rawtextbasepath/bible/"$l_short" > "$l_short".bible.tokf
+    python generate_features.py  --train_file $treebankbasepath/"$l_short"-ud-all.conllu.lex --to_tok $rawtextbasepath/watchtower/"$l_short" > "$l_short".watchtower.tokf
     rungsted --train "$l_short".all.train.tokf -f $l_short.tok.mdl
     rungsted --test "$l_short".watchtower.tokf -i $l_short.tok.mdl --predictions $l_short".watchtower.tpred"
     rungsted --test "$l_short".bible.tokf -i $l_short.tok.mdl --predictions $l_short".bible.tpred"
