3b8402f6c6dd95a064970bd0b1e2bdaa32f3afaf
alonso <hector.martinez.a@gmail.com>
2015-12-17 13:50:42 +0100

    searn

--- src/train_test_all_searn.sh
+++ src/train_test_all_searn.sh
@@ -15,7 +15,7 @@ l_short=${lang_short[$v]}
     #python generate_features.py  --train_file $treebankbasepath/"$l_short"-ud-train.conllu.lex --test_file $treebankbasepath/"$l_short"-ud-test.conllu.lex > "$l_short".test.tokf
     #python generate_features.py  --train_file $treebankbasepath/"$l_short"-ud-train.conllu.lex --test_file $treebankbasepath/"$l_short"-ud-dev.conllu.lex > "$l_short".dev.tokf
     #python generate_features.py --train_file $treebankbasepath/"$l_short"-ud-train.conllu.lex > "$l_short".train.tokf
-    /home/alonso/tool/vowpal_wabbit/vowpalwabbit/vw -d $featsbasepath/$l_short.train.tokf -f $featsbasepath/$l_short.tok.mdl --search_task sequence
+    /home/alonso/tool/vowpal_wabbit/vowpalwabbit/vw -d $featsbasepath/$l_short.train.tokf -f $featsbasepath/$l_short.tok.mdl --search 2 --search_task sequence
     /home/alonso/tool/vowpal_wabbit/vowpalwabbit/vw  -t -d $featsbasepath/$l_short.test.tokf  -i $featsbasepath/$l_short.tok.mdl -p $featsbasepath/$l_short.test.tpred
     python eval_searn.py --pred $featsbasepath/$l_short.test.pred --feats $featsbasepath/$l_short.test.tokf
     /home/alonso/tool/vowpal_wabbit/vowpalwabbit/vw  -t -d $featsbasepath/$l_short.dev.tokf  -i $featsbasepath/$l_short.tok.mdl -p $featsbasepath/$l_short.dev.tpred
