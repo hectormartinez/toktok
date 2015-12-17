d23e26dd03b517b56f6802a646821e75c1bb2c06
alonso <hector.martinez.a@gmail.com>
2015-12-17 14:41:22 +0100

    gen

--- src/generate_features.py
+++ src/generate_features.py
@@ -107,7 +107,7 @@ class FeatureInstance:
 
 def read_token_per_line_sentences(infile,column):
     sent = []
-    for line in open(infile):
+    for line in open(infile,encoding="utf-8").readlines():
         line = line.strip()
         if not line:
             yield sent
